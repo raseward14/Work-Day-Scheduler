835fd4c20bd18e4e7ad306180e7fd31e8350ddbf
Richard Seward <raseward14@gmail.com>
2021-01-30 09:24:25 -0700

    added object storedTodo in local storage

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -7,12 +7,10 @@ $(document).ready(function () {
 
     //standard biz hours are 8am-5pm- 9 blocks
     var workingHours = ['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
-    
-    // var currentHour = parseInt(moment().format('HH'));
-    var currentHour = 13;
+
+    var currentHour = parseInt(moment().format('HH'));
+    // var currentHour = 13;
     console.log(currentHour);
-
-
 
     // create element p item for time blocks for that day
     for (let i = 0; i < workingHours.length; i++) {
@@ -39,57 +37,67 @@ $(document).ready(function () {
         $(input).attr('data-hour', hr);
     }
 
-    $('.textarea').each(function(index, input) {
+    $('.textarea').each(function (index, input) {
         var hour = parseInt($(this).attr('data-hour'));
         console.log(typeof hour, typeof currentHour);
         if (hour < currentHour) {
             $(this).removeClass("future");
             $(this).removeClass("present");
             $(this).addClass("past");
-        } 
+        }
         else if (hour === currentHour) {
             $(this).removeClass("future");
             $(this).removeClass("past");
             $(this).addClass("present");
-        } 
+        }
         else {
             $(this).removeClass("past");
             $(this).removeClass("present");
             $(this).addClass("future");
         }
     });
-    
-    
-    $(".saveBtn").on("click", function(event) {
+
+    // array of storedtodos
+    // var storedTodos = [];
+    // console.log(storedTodos);
+
+    // print todos onto page when refreshed
+    function renderTodos() {
+        // value of todoInput printed to input element value
+        // each todo will be one of the storedTodos items indexes
+        var storedTodos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    // on click event for timeblocks
+    $(".saveBtn").on("click", function (event) {
         event.preventDefault();
 
-        var comment = document.querySelector('.textarea');
+        // store input value
+        var todoInput = $(this).siblings('input').val();
+        console.log("The todo is = " + todoInput);
 
-        var todo = {
-            time: hour.textContent,
-            item: input.textarea.textContent
-        }
-        localStorage.setItem('input', JSON.stringify(todo));
+        // todo input is pushed onto stored todos array
+        // storedTodos.push(todoInput);
 
-        renderAgenda();
+        // stored hour value
+        var hour = $(this).siblings('p').text();
+        console.log("The hour is = " + hour);
+
+        // object with input value and hour
+        var storedTodos = {
+            time: hour,
+            item: todoInput
+        };    
+
+        // store all todos that have been pushed to the array
+        localStorage.setItem('todos', JSON.stringify(storedTodos));
+
+        renderTodos();
 
     });
 
-    function renderAgenda() {
-
-        var todos = JSON.parse(localStorage.getItem('input'));
-    }
-
+    console.log(document.body);
 });
-    // append to the previous li item
-    // for loop see activity 8 lotto generator
 
-    // color code time blocks
-    // grey for past, red for current, green for future
 
-    // on click event for timeblocks
-    // input an event
-    // on click save button
-    // txt for this timeblock saved to local storage
-    // see week 4 local storage set item
     // saved events stay in local storage after refreshing page
